de027c3abca3f4376543724cbb295f3632e1c242
khaa-dv <59757815+khaa-dv@users.noreply.github.com>
2021-09-19 22:44:15 +1000

    Update pre_install.py

--- pre_install.py
+++ pre_install.py
@@ -2,4 +2,4 @@ from config import URL_HOST, POSTGRES_PORT
 import os
 
 os.system('docker pull postgres')
-os.system('docker run --name postgres_az -p '+POSTGRES_PORT+':'+POSTGRES_PORT+' -e POSTGRES_PASSWORD=postgres -d postgres:latest')
+os.system('docker run --name postgres_az -p '+POSTGRES_PORT+':5432 -e POSTGRES_PASSWORD=postgres -d postgres:latest')
